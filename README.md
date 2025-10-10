08e35daa0f57e8190ddf71d252efbf54b09937dd
franCordobaDeveloper <franCordobaDeveloper>
2025-10-10 20:24:20 -0300

    filtrar productos por precio, nombre y talles completo

---
+++ README.md
@@ -0,0 +1,7 @@
+#TesloShop
+
+1. Clonar el repositorio.
+2 .Clonar el archivo .env.template y renombralo a .env
+3. Ejecutar el comando `npm install` para instalar la dependencias
+4. Verificar que el backend este corriendo el puerto 3000
+5. Ejucutar el comando `npm run dev` para iniciar el servidor en desarrollo
---
+++ src/TesloShopApp.tsx
@@ -0,0 +1,18 @@
+import { RouterProvider } from "react-router"
+import { appRouter } from "./app.router"
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient();
+
+export const TesloShopApp = () => {
+    return (
+
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={appRouter} />
+            {/* The rest of your application */}
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+
+    );
+};
