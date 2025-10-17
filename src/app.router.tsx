28abc6eed62268f8cd4fe95080c01f7dbfeb9370
franCordobaDeveloper <franCordobaDeveloper>
2025-10-17 19:26:07 -0300

    add createHashRouter

--- src/app.router.tsx
+++ src/app.router.tsx
@@ -1,5 +1,5 @@
 import { lazy } from 'react';
-import { createBrowserRouter, Navigate } from 'react-router';
+import { createBrowserRouter, createHashRouter, Navigate } from 'react-router';
 
 import { ShopLayout } from './shop/layouts/ShopLayout';
 import { HomePage } from './shop/pages/home/HomePage';
@@ -18,7 +18,7 @@ import { AdminRoute, NotAuthenticatedRoute } from './components/routes/Protected
 const AuthLayout = lazy(() => import('./auth/layouts/AuthLayout'));
 const AdminLayout = lazy(() => import('./admin/layout/AdminLayout'));
 
-export const appRouter = createBrowserRouter([
+export const appRouter = createHashRouter([
     // Main routes
     {
         path: '/',
